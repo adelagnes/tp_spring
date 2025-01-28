1fa5b47e433c962a5d4ca849f7f1a0dc47cec286
Antoine Delagnes <antoine.delagnes@etu.iut-tlse3.fr>
2025-01-28 21:41:42 +0100

    feat : intial commit

---
+++ src/main/java/Spring_rendu/SpringRenduApplication.java
@@ -0,0 +1,13 @@
+package Spring_rendu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRenduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRenduApplication.class, args);
+    }
+
+}
