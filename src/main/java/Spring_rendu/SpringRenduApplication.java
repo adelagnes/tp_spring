e63d716b12733c54ad90a5ca0c5973c0019d50d4
Antoine Delagnes <antoine.delagnes@etu.iut-tlse3.fr>
2025-01-29 23:20:25 +0100

    feat : test postman fait

--- src/main/java/Spring_rendu/SpringRenduApplication.java
+++ src/main/java/Spring_rendu/SpringRenduApplication.java
@@ -1,9 +1,9 @@
 package Spring_rendu;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = "Spring_rendu.model")
